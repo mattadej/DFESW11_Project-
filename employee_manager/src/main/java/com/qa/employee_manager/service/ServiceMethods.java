02f31dfca7cdd7631923addfb0361b1d9e5b66e2
Matthew Adejumo <mattadeju@gmail.com>
2022-03-16 13:18:29 +0000

    interface that I will use to implement CRUD functionality

--- employee_manager/src/main/java/com/qa/employee_manager/service/ServiceMethods.java
+++ employee_manager/src/main/java/com/qa/employee_manager/service/ServiceMethods.java
@@ -1,1 +1,22 @@
+package com.qa.employee_manager.service;
 
+import java.util.List;
+
+public interface ServiceMethods<T> {
+
+    //Create
+    T create(T employee);
+
+    //Read All
+    List<T> realAll();
+
+    //Read By ID
+    T readById(long id);
+
+    //Update
+    T update(long id, T employee);
+
+    //Delete
+    boolean delete(long id);
+
+}
