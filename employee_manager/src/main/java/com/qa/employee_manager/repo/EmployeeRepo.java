ad2732ade26a1a041ac10f027e3986e4d32e9592
Matthew Adejumo <mattadeju@gmail.com>
2022-03-16 23:09:00 +0000

    Added relevant annotation and import

--- employee_manager/src/main/java/com/qa/employee_manager/repo/EmployeeRepo.java
+++ employee_manager/src/main/java/com/qa/employee_manager/repo/EmployeeRepo.java
@@ -2,10 +2,12 @@ package com.qa.employee_manager.repo;
 
 import com.qa.employee_manager.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 //The reason for this class is to allow us to have access to
 //repository methods because they come from the JpaRepository interface.
 
+@Repository
 public interface EmployeeRepo extends JpaRepository<Employee, Long> {
 
 }
